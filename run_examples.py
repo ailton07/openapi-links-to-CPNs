b28e15b268aefde6cc6db883dbb3b01627211055
Ailton Santos <ailton07@yahoo.com.br>
2022-12-03 12:01:41 -0400

    lint: describe tests

--- run_examples.py
+++ run_examples.py
@@ -1,19 +1,33 @@
 import os
 
-OPENAPI_PATH, LOGS_PATH = 'examples/Structural_Problem_Based_on_BOLA_Example.yaml', 'logs/combined_example_structural_problem.json'
+EXECUTABLE = "execute_replay.py"
+# strutuctural problem based on BOLA_Example, that is the same endpoint POST /login with 2 different resulting status codes
+#OPENAPI_PATH, LOGS_PATH = 'examples/Structural_Problem_Based_on_BOLA_Example.yaml', 'logs/combined_example_structural_problem.json'
+
+# strutuctural problem based on BOLA_Example, /login accepting POST and PUT methods. /accounts/{id} is accepting GET
 #OPENAPI_PATH, LOGS_PATH = 'examples/Structural_Problem_Based_on_BOLA_Example_02.yaml', 'logs/combined_example_structural_problem.json'
+
+# strutuctural problem based on BOLA_Example, /login accepting POST and PUT methods. /accounts is accepting POST
 #OPENAPI_PATH, LOGS_PATH = 'examples/Structural_Problem_Based_on_BOLA_Example_03.yaml', 'logs/combined_example_structural_problem_example_03.json'
+
+# BOLA example, we have /login and /accounts/{id}. Logs describing a no attack scenario
 #OPENAPI_PATH, LOGS_PATH = 'examples/BOLA_Example_login_and_accounts_id.yaml', 'logs/combined_bola_example.json'
 
 # Juice Shop Cases
+# /rest/basket/{bid} and /rest/user/login, no attack scenario
 #OPENAPI_PATH, LOGS_PATH = 'examples/JuiceShop.yaml', 'logs/combined_login.json'
-#OPENAPI_PATH, LOGS_PATH = 'examples/JuiceShop.yaml', 'logs/combined_login_multiuser.json'
-#OPENAPI_PATH, LOGS_PATH = 'examples/JuiceShop.yaml', 'logs/log_signup_login_basket_with_false_positive.log'
-#OPENAPI_PATH, LOGS_PATH = 'examples/JuiceShop.yaml', 'logs/combined_logs_juice_shop_signup_view_basket.log'
-# a vulnerabilidade é explorada por HTTP Parameter Pollution
-#OPENAPI_PATH, LOGS_PATH = 'examples/OWASP-Juice-Shop-BOLA-cases.yaml', 'logs/combined_logs_Juic_Shop_manipulate_basket.log'
-EXECUTABLE = "execute_replay.py"
 
+# /rest/basket/{bid} and /rest/user/login, no attack scenario with multiple users
+#OPENAPI_PATH, LOGS_PATH = 'examples/JuiceShop.yaml', 'logs/combined_login_multiuser.json'
+
+# /rest/basket/{bid} and /rest/user/login, no attack scenario, but app running in AWS
+#OPENAPI_PATH, LOGS_PATH = 'examples/JuiceShop.yaml', 'logs/real_log_signup_login_basket.log.log'
+
+# /rest/basket/{bid} and /rest/user/login, attack scenario view basket, but app running in AWS
+#OPENAPI_PATH, LOGS_PATH = 'examples/JuiceShop.yaml', 'logs/real_logs_juice_shop_signup_view_basket.log'
+
+# /rest/basket/{bid} and /rest/user/login, attack exploring HTTP Parameter Pollution
+OPENAPI_PATH, LOGS_PATH = 'examples/OWASP-Juice-Shop-BOLA-cases.yaml', 'logs/combined_logs_Juic_Shop_manipulate_basket.log'
 
 
 def main():
