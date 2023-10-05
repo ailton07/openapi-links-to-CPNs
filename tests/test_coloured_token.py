ee6ade60b19866e6b25dfea79d1a98e6829cef33
Ailton da Silva <ailton07@yahoo.com.br>
2023-10-05 15:23:09 -0300

    improve tests

--- tests/test_coloured_token.py
+++ tests/test_coloured_token.py
@@ -16,11 +16,14 @@ def get_logs(logs_name):
 
 @pytest.mark.parametrize("logs_name, line, expected_result", [
     # test with two parts item (authentication.umail)
-    ("logs/combined_example_structural_problem.json", 1, 
+    ("tests/logs/combined_example_structural_problem.json", 1, 
      {'authentication.umail':'email@email.com'}),
     # test with one part item (status)
-     ("logs/combined_example_structural_problem.json", 3, 
-     {'status':'success'})     
+     ("tests/logs/combined_example_structural_problem.json", 3, 
+     {'status':'success'}),
+     # test with response_body contenting a list
+     ("tests/logs/memos_login_create_archieve.json", 0,
+     {'id':['4', '4']})
 ])
 def test_get_from_reponse_body(logs_name, line, expected_result, get_logs):
     log_line = get_logs[line]
@@ -29,6 +32,20 @@ def test_get_from_reponse_body(logs_name, line, expected_result, get_logs):
         key, value = request_line._get_from_reponse_body(key_value)
         assert value is not None 
         assert value == expected_value
+
+
+@pytest.mark.parametrize("logs_name, line, expected_result", [
+     # test with response contenting an empty list
+     ("tests/logs/memos_login_create_archieve.json", 1,
+     {'response_body':'success'})
+])
+def test_get_from_reponse_body_with_exception(logs_name, line, expected_result, get_logs):
+    log_line = get_logs[line]
+    request_line = RequestResponseToken(*LogUtils.create_request_response_from_log(log_line))
+    for key_value, expected_value in expected_result.items():
+        with pytest.raises(Exception) as exception:
+            key, value = request_line._get_from_reponse_body(key_value)
+        assert exception is not None 
 
 
 @pytest.mark.parametrize("logs_name, line, test_value, expected_result", [
